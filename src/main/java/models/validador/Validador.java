7b01af8fccfcd7c8968259780cb7e7000d53d5ce
Agustin Ranieri <aguseranieri@gmail.com>
2022-05-14 23:02:42 -0300

    actualizo Validador

--- src/main/java/models/validador/Validador.java
+++ src/main/java/models/validador/Validador.java
@@ -1,6 +1,7 @@
 package models.validador;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import exceptions.ContrasenaDebilException;
 
@@ -17,7 +18,9 @@ public class Validador {
   
   public void validar(String usuario, String contrasena) {
     String errores = validaciones.stream()
-        .map(validacion -> validacion.validar(usuario, contrasena).orElse(""))
+        .map(validacion -> validacion.validar(usuario, contrasena))
+        .filter(Optional::isPresent)
+        .map(Optional::get)
         .collect(Collectors.joining());
 
     if (!errores.isEmpty()) {
