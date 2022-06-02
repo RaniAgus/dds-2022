5545526d43688bfe1f73c588e7adb0cd17edb412
Agustin Ranieri <aguseranieri@gmail.com>
2022-06-01 21:46:46 -0300

    muevo test de alta de trayectos a TrayectoTest

--- src/test/java/models/TrayectoTest.java
+++ src/test/java/models/TrayectoTest.java
@@ -1,6 +1,5 @@
 package models;
 import models.geolocalizacion.Distancia;
-import models.geolocalizacion.Ubicacion;
 import models.geolocalizacion.Unidad;
 import models.miembro.Miembro;
 import models.miembro.Tramo;
@@ -8,14 +7,24 @@ import models.miembro.Trayecto;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static models.factory.MiembroFactory.agus;
+import static models.factory.TramoFactory.tramoAPieDesdeMedranoHastaAlem;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TrayectoTest {
+  @Test
+  public void sePuedeDarDeAltaUnNuevoTrayecto() {
+    Miembro miembro = agus();
+    Trayecto trayecto = new Trayecto(singletonList(tramoAPieDesdeMedranoHastaAlem()), singletonList(miembro));
+    assertThat(trayecto.getViajantes()).containsExactly(miembro);
+  }
+
   @Test
   public void laDistanciaDeUnTrayectoEsLaSumaDeSusTramos() {
     Tramo primerTramo = mock(Tramo.class);
@@ -25,7 +34,7 @@ public class TrayectoTest {
     when(primerTramo.getDistancia()).thenReturn(new Distancia(new BigDecimal(50), Unidad.KM));
     when(segundoTramo.getDistancia()).thenReturn(new Distancia(new BigDecimal(200), Unidad.KM));
 
-    Trayecto trayectoTest = new Trayecto(asList(primerTramo,segundoTramo),mock(List.class));
-    assertEquals(250,trayectoTest.medirDistanciaTrayecto().getValor());
+    Trayecto trayectoTest = new Trayecto(asList(primerTramo, segundoTramo), emptyList());
+    assertEquals(250, trayectoTest.medirDistanciaTrayecto().getValor());
   }
 }
