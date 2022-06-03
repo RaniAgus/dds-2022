8585a7e7bf2da0cccf5b072c578ec34c9211d9a2
Marco Piatti <marcopiatticastro@hotmail.com>
2022-06-03 17:49:31 -0300

    tests  linea agregarParada

--- src/test/java/models/LineaTest.java
+++ src/test/java/models/LineaTest.java
@@ -1,11 +1,15 @@
 package models;
 
+import models.geolocalizacion.Distancia;
+import models.geolocalizacion.Unidad;
 import models.mediodetransporte.Linea;
 import models.mediodetransporte.Parada;
 import org.junit.jupiter.api.Test;
 import static models.factory.LineaFactory.subteB;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+
+import java.math.BigDecimal;
 
 public class LineaTest {
 
@@ -14,8 +18,20 @@ public class LineaTest {
     Linea linea = subteB();
     Parada nuevaParada = mock(Parada.class);
 
-    linea.agregarParada(nuevaParada);
+    linea.agregarParada(nuevaParada, new Distancia(BigDecimal.valueOf(0), Unidad.KM));
 
     assertThat(linea.getParadas()).contains(nuevaParada);
+  }
+
+  @Test
+  public void alDarDeAltaUnaParadaSeCambiaLaDistanciaDeLaAnterior() {
+    Linea linea = subteB();
+    Parada primerParada = new Parada(new Distancia(BigDecimal.valueOf(0), Unidad.KM), "primera");
+    linea.agregarParada(primerParada, new Distancia(BigDecimal.valueOf(0), Unidad.KM));
+
+    Parada segundaParada = new Parada(new Distancia(BigDecimal.valueOf(0), Unidad.KM), "primera");
+    linea.agregarParada(segundaParada, new Distancia(BigDecimal.valueOf(50), Unidad.KM));
+
+    assertThat(primerParada.getDistanciaAProximaParada().getValor()).isEqualTo(50);
   }
 }
