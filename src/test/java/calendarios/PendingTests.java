5a21c7e507b0cc9d2c84c77c9ab551b3f28cdd29
Agustin Ranieri <aguseranieri@gmail.com>
2022-06-19 03:57:25 -0300

    elimino clase de tests en desuso

--- src/test/java/calendarios/PendingTests.java
+++
@@ -1,6 +0,0 @@
-package calendarios;
-
-public class PendingTests extends CalendariosTest {
-  // 9. Permitir asignarle a un evento varios recordatorios, que se enviarán cuando falte un cierto tiempo
-
-}
