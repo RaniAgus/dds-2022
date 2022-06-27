2a2649c670b9a17f698f058a403fea899ab7044e
Agustin Ranieri <aguseranieri@gmail.com>
2022-06-27 17:38:14 -0300

    test del lector de archivos posta

---
+++ src/test/java/models/LectorDeArchivosTest.java
@@ -0,0 +1,19 @@
+package models;
+
+import models.da.LectorDeArchivos;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LectorDeArchivosTest extends BaseTest {
+  @Test
+  void sePuedeLeerUnArchivo() {
+    LectorDeArchivos lector = new LectorDeArchivos("./src/test/resources/DA_correcto.csv");
+
+    assertThat(lector.getLineas()).containsExactly(
+        "TIPO_CONSUMO;VALOR;PERIODICIDAD;PERIODO",
+        "ELECTRICIDAD;122;MENSUAL;03/2002",
+        "NAFTA;5;ANUAL;2020"
+    );
+  }
+}
