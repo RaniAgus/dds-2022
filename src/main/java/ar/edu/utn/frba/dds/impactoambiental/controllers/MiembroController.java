a583649838f45e328d4fa4cfce9c538fc3f897bd
Marco Piatti <marcopiatticastro@hotmail.com>
2022-10-31 17:04:43 -0300

    agrego controller para generar vinculacion

--- src/main/java/ar/edu/utn/frba/dds/impactoambiental/controllers/MiembroController.java
+++ src/main/java/ar/edu/utn/frba/dds/impactoambiental/controllers/MiembroController.java
@@ -1,13 +1,19 @@
 package ar.edu.utn.frba.dds.impactoambiental.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableMap;
 
 import ar.edu.utn.frba.dds.impactoambiental.dtos.VinculacionDto;
-import ar.edu.utn.frba.dds.impactoambiental.models.organizacion.Organizacion;
+import ar.edu.utn.frba.dds.impactoambiental.models.forms.Form;
+import ar.edu.utn.frba.dds.impactoambiental.models.miembro.Miembro;
+import ar.edu.utn.frba.dds.impactoambiental.models.miembro.Trayecto;
+import ar.edu.utn.frba.dds.impactoambiental.models.organizacion.Sector;
+import ar.edu.utn.frba.dds.impactoambiental.models.organizacion.Vinculacion;
 import ar.edu.utn.frba.dds.impactoambiental.models.usuario.UsuarioMiembro;
 import ar.edu.utn.frba.dds.impactoambiental.repositories.RepositorioOrganizaciones;
 import ar.edu.utn.frba.dds.impactoambiental.repositories.RepositorioUsuarios;
@@ -15,11 +21,15 @@ import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
-public class MiembroController {
+public class MiembroController implements Controlador{
+
+  //Esto esta feo pero es un lindo placeholder
+  public UsuarioMiembro obtenerUsuario(Request req) {
+    return (UsuarioMiembro) RepositorioUsuarios.getInstance().obtenerPorID(req.session().attribute("usuario")).get();
+  }
+
   public ModelAndView vinculaciones(Request request, Response response) {
-    UsuarioMiembro usuario = RepositorioUsuarios.getInstance()
-      .obtenerPorID(request.session().attribute("usuario_id")).get(); 
-    //ver como solucionar casteo usuario -> usuarioMiembro
+    UsuarioMiembro usuario = obtenerUsuario(request);
 
     List<VinculacionDto> vinculaciones = usuario.getMiembros().stream()
       .map(m -> new VinculacionDto(
@@ -44,10 +54,32 @@ public class MiembroController {
   }
 
   public ModelAndView proponerVinculacion(Request request, Response response) {
-    return null;
+    UsuarioMiembro usuario = obtenerUsuario(request);
+
+    return Form.of(request).getParamOrError("codigoInvite", "Es requerido un codigo")
+      .map(UUID::fromString, "El codigo ingresado no tiene el formato correcto")
+      .map(u -> RepositorioOrganizaciones.getInstance().obtenerTodos().stream()
+        .flatMap(o -> o.getSectores().stream()
+          .filter(s -> s.getCodigoInvite().equals(u))).findFirst().get()
+        , "El codigo ingresado no existe")
+      .fold(
+        errores -> {
+          response.redirect("/miembro/" + usuario.getId() + "/vinculaciones?errores=" + encode(String.join(", ", errores)));
+          return null;
+        },
+        sector -> {
+          Miembro membersia = new Miembro(new ArrayList<>());
+          sector.solicitarVinculacion(new Vinculacion(membersia));
+          usuario.agregarMiembro(membersia);
+
+          response.redirect("/");
+          return null;
+        }
+      );
   }
 
   public ModelAndView trayectos(Request request, Response response) {
+    
     return null;
   }
 
