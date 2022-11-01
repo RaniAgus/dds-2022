a189d3487e7b9d6fd646a784342c73409df5da40
Marco Piatti <marcopiatticastro@hotmail.com>
2022-11-01 13:22:43 -0300

    constructor para dtos tramo

--- src/main/java/ar/edu/utn/frba/dds/impactoambiental/dtos/TramoDto.java
+++ src/main/java/ar/edu/utn/frba/dds/impactoambiental/dtos/TramoDto.java
@@ -1,5 +1,10 @@
 package ar.edu.utn.frba.dds.impactoambiental.dtos;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ar.edu.utn.frba.dds.impactoambiental.models.miembro.Tramo;
+
 public class TramoDto {
   String tipo;
   String medio;
@@ -11,6 +16,13 @@ public class TramoDto {
     this.medio = medio;
     this.origen = origen;
     this.destino = destino;
+  }
+
+  public TramoDto(Tramo tramo) {
+    this.tipo = tramo.tipo();
+    this.medio = tramo.nombreMedio();
+    this.origen = tramo.nombreOrigen();
+    this.destino = tramo.nombreDestino();
   }
 
   public String getTipo() {
@@ -43,5 +55,9 @@ public class TramoDto {
 
   public void setDestino(String destino) {
     this.destino = destino;
+  }
+
+  public static List<TramoDto> ofList(List<Tramo> tramos) {
+    return tramos.stream().map(TramoDto::new).collect(Collectors.toList());
   }
 }
