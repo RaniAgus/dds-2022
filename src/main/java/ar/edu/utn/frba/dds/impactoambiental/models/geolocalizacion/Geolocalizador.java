8287b17bdacdb21bab8c7c0ea624ab431ebbfcd3
Marco Piatti <marcopiatticastro@hotmail.com>
2022-11-01 13:22:35 -0300

    agrego funcionalidad para crear ubicaciones a partir de strings

--- src/main/java/ar/edu/utn/frba/dds/impactoambiental/models/geolocalizacion/Geolocalizador.java
+++ src/main/java/ar/edu/utn/frba/dds/impactoambiental/models/geolocalizacion/Geolocalizador.java
@@ -8,6 +8,7 @@ import retrofit2.Response;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 public class Geolocalizador {
@@ -21,17 +22,54 @@ public class Geolocalizador {
     return consultar(GeoddsApi.INSTANCE.getPaises(1, apiKey));
   }
 
+  public Optional<Pais> findPais(String nombre) {
+    return getPaises().stream()
+        .filter(pais -> pais.getNombre().equals(nombre))
+        .findFirst();
+  }
+
   public List<Provincia> getProvincias(Pais pais) {
     return consultar(GeoddsApi.INSTANCE.getProvincias(1, pais.getId(), apiKey));
+  }
+
+  public Optional<Provincia> findProvincia(Pais pais, String nombre) {
+    return getProvincias(pais).stream()
+        .filter(provincia -> provincia.getNombre().equals(nombre))
+        .findFirst();
   }
 
   public List<Municipio> getMunicipios(Provincia provincia) {
     return paginar(offset -> GeoddsApi.INSTANCE.getMunicipios(offset, provincia.getId(), apiKey));
   }
 
+  public Optional<Municipio> findMunicipio(Provincia provincia, String nombre) {
+    return getMunicipios(provincia).stream()
+        .filter(municipio -> municipio.getNombre().equals(nombre))
+        .findFirst();
+  }
+
   public List<Localidad> getLocalidades(Municipio municipio) {
     return paginar(offset -> GeoddsApi.INSTANCE.getLocalidades(offset, municipio.getId(), apiKey));
   }
+
+  public Optional<Localidad> findLocalidad(Municipio municipio, String nombre) {
+    return getLocalidades(municipio).stream()
+        .filter(localidad -> localidad.getNombre().equals(nombre))
+        .findFirst();
+  }
+
+  public Optional<Ubicacion> getUbicacion(String pais, 
+                                          String provincia, 
+                                          String municipio, 
+                                          String localidad,
+                                          String calle,
+                                          String altura) {
+      return findPais(pais)
+        .flatMap(p -> findProvincia(p, provincia))
+        .flatMap(pr -> findMunicipio(pr, municipio))
+        .flatMap(mu -> findLocalidad(mu, localidad))
+        .map(lo -> new Ubicacion(lo.getId(), calle, altura));
+    }
 
   public Distancia medirDistancia(Ubicacion ubicacionOrigen, Ubicacion ubicacionDestino) {
     return consultar(GeoddsApi.INSTANCE.getDistancia(
