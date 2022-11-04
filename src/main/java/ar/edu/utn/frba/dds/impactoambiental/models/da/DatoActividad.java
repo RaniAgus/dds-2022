aa24c329b3359d09e4391cedf3c1dd1947b1c369
Agustin Ranieri <aguseranieri@gmail.com>
2022-11-04 17:52:21 -0300

    Tp terminado

--- src/main/java/ar/edu/utn/frba/dds/impactoambiental/models/da/DatoActividad.java
+++ src/main/java/ar/edu/utn/frba/dds/impactoambiental/models/da/DatoActividad.java
@@ -1,7 +1,6 @@
 package ar.edu.utn.frba.dds.impactoambiental.models.da;
 
 import ar.edu.utn.frba.dds.impactoambiental.models.EntidadPersistente;
-
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -32,7 +31,7 @@ public class DatoActividad extends EntidadPersistente {
   }
 
   public Boolean estaEnPeriodo(Periodo periodo) {
-    return periodo.estaEnPeriodo(periodo);
+    return this.periodo.estaEnPeriodo(periodo);
   }
 
   public Boolean tieneTipoDeConsumo(TipoDeConsumo tipo) {
